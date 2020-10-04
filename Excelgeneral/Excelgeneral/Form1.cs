51aaebd8d6396a2440a9a776f262e66ebc9a9284
Botond Vigh <vbotinho@gmail.com>
2020-10-04 14:21:02 +0200

    Munkafüzet létrehozás és megnyitás

--- Excelgeneral/Excelgeneral/Form1.cs
+++ Excelgeneral/Excelgeneral/Form1.cs
@@ -18,6 +18,9 @@ namespace Excelgeneral
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
         public Form1()
         {
             InitializeComponent();
@@ -32,6 +35,41 @@ namespace Excelgeneral
 
         }
 
+        private void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+
+
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+
+                xlSheet = xlWB.ActiveSheet;
+
+                  CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+
+        private void CreateTable()
+        {
+
+        }
     
     }
 }
