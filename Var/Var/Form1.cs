5e7c13dddc69db5383ed68538c4144d5a902c5c5
Botond Vigh <vbotinho@gmail.com>
2020-10-10 19:57:12 +0200

    Adatbetöltés

--- Var/Var/Form1.cs
+++ Var/Var/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Var.Entities;
 
 namespace Var
 {
@@ -17,13 +18,25 @@ namespace Var
     {
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
         public Form1()
         {
             InitializeComponent();
             List<Tick> l = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
+
+        }
 
 
+        private void CreatePortfolio()
+        {
+            
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
