7aa75e09d2cec16115953ca62da6f39adea6a193
Botond Vigh <vbotinho@gmail.com>
2020-10-10 18:50:29 +0200

    adat betöltés

--- Var/Var/Form1.cs
+++ Var/Var/Form1.cs
@@ -10,11 +10,20 @@ using System.Windows.Forms;
 
 namespace Var
 {
+    
+
+
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
         public Form1()
         {
             InitializeComponent();
+            List<Tick> l = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
+
+
         }
     }
 }
