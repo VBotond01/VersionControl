d402ded84842473a41b402e9ee1ae8aa99e6a420
Botond Vigh <vbotinho@gmail.com>
2020-10-11 14:17:12 +0200

    Fájlba mentés

--- Var/Var/Form1.cs
+++ Var/Var/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -82,8 +83,30 @@ namespace Var
 
         }
 
-
-
-
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.InitialDirectory = Application.StartupPath;
+            sfd.Filter = "Comma Seperated Values (*.csv)|*.csv";
+            sfd.DefaultExt = "csv";
+            sfd.AddExtension = true;
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                sw.Write("Időszak");
+                sw.Write(";");
+                sw.Write("Nyereség");
+                sw.WriteLine();
+                int counter = 0;
+                foreach (var x in Nyereségek)
+                {
+                    sw.Write(counter);
+                    sw.Write(";");
+                    sw.Write(x);
+                    sw.WriteLine();
+                    counter++;
+                }
+            }
+        }
     }
 }
