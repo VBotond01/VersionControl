29059f8fc577aee96158fbe0b0340ec55b8e34e2
Botond Vigh <vbotinho@gmail.com>
2020-10-11 13:35:50 +0200

    Var Kiszámítás

--- Var/Var/Form1.cs
+++ Var/Var/Form1.cs
@@ -19,12 +19,32 @@ namespace Var
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
         List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+        List<decimal> Nyereségek;
         public Form1()
         {
             InitializeComponent();
-            List<Tick> l = context.Ticks.ToList();
+            Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
             CreatePortfolio();
+
+            Nyereségek = new List<decimal>();
+            int intervalum = 30;
+            DateTime kezdőDátum = (from x in Ticks select x.TradingDay).Min();
+            DateTime záróDátum = new DateTime(2016, 12, 30);
+            TimeSpan z = záróDátum - kezdőDátum;
+            for (int i = 0; i < z.Days - intervalum; i++)
+            {
+                decimal ny = GetPortfolioValue(kezdőDátum.AddDays(i + intervalum))
+                           - GetPortfolioValue(kezdőDátum.AddDays(i));
+                Nyereségek.Add(ny);
+                Console.WriteLine(i + " " + ny);
+            }
+
+            var nyereségekRendezve = (from x in Nyereségek
+                                      orderby x
+                                      select x)
+                                        .ToList();
+            MessageBox.Show(nyereségekRendezve[nyereségekRendezve.Count() / 5].ToString());
 
         }
 
@@ -37,6 +57,8 @@ namespace Var
             Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
 
             dataGridView2.DataSource = Portfolio;
+
+
         }
         private decimal GetPortfolioValue(DateTime date)
         {
@@ -49,8 +71,15 @@ namespace Var
                             select x)
                             .First();
                 value += (decimal)last.Price * item.Volume;
+
+
             }
             return value;
+
+
+
+
+
         }
 
 
