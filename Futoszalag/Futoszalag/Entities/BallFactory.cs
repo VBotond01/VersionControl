209b5423d8069c73b38a0fe3ce0b85852228fde5
Botond Vigh <vbotinho@gmail.com>
2020-11-15 21:04:20 +0100

    Gyár absztrakció és felhasználása gyárakban

--- Futoszalag/Futoszalag/Entities/BallFactory.cs
+++ Futoszalag/Futoszalag/Entities/BallFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Futoszalag.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,9 @@ using System.Threading.Tasks;
 
 namespace Futoszalag.Entities
 {
-    public class BallFactory
+    public class BallFactory : IToyFactory
     {
-        public Ball CreateNew()
+        public Toy CreateNew()
         {
             return new Ball();
         }
