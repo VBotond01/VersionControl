3bc549db3bd86a0938f256c8c6c48844c88f6b1f
Botond Vigh <vbotinho@gmail.com>
2020-11-15 21:29:51 +0100

    Absztrackió felhasználása futószalag

--- Futoszalag/Futoszalag/Abstractions/IToyFactory.cs
+++ Futoszalag/Futoszalag/Abstractions/IToyFactory.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Futoszalag.Abstractions
 {
-    interface IToyFactory
+    public interface IToyFactory
     {
         Toy CreateNew();
     }
---
+++ Futoszalag/Futoszalag/Entities/CarFactory.cs
@@ -0,0 +1,18 @@
+﻿using Futoszalag.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Futoszalag.Entities
+{
+    class CarFactory : IToyFactory
+    {
+        public Toy CreateNew()
+        {
+            return new Car();
+
+        }
+    }
+}
