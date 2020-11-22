a76a69dae538a83d4a6093790323d303707fcdab
Botond Vigh <vbotinho@gmail.com>
2020-11-22 21:56:44 +0100

    Függvény validálás kivétel + javítások

--- UnitTestExample (1)/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample (1)/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -13,19 +13,13 @@ namespace UnitTestExample.Test
 {
     class AccountControllerTestFixture
     {
-        [Test, 
-        TestCase("abcd1234", false),
-        TestCase("irf@uni-corvinus", false),
-        TestCase("irf.uni-corvinus.hu", false),
-        TestCase("irf@uni-corvinus.hu", true),
-        TestCase("abcdefgh", false),
-            TestCase("ABCD1234", false),
-            TestCase("abcd", false),
-            TestCase("AbcD1234", true),
-        TestCase("irf@uni-corvinus.hu", "Abcd1234"),
-        TestCase("irf@uni-corvinus.hu", "Abcd1234567"),
-
-            ]
+        [
+    Test,
+    TestCase("abcd1234", false),
+    TestCase("irf@uni-corvinus", false),
+    TestCase("irf.uni-corvinus.hu", false),
+    TestCase("irf@uni-corvinus.hu", true)
+]
 
         public void TestValidateEmail(string email, bool expectedResult)
         {
@@ -36,6 +30,30 @@ namespace UnitTestExample.Test
 
         }
 
+        [
+    Test,
+    TestCase("abcdefgh", false),
+    TestCase("ABCD1234", false),
+    TestCase("abcd1234", false),
+    TestCase("abcd", false),
+    TestCase("AbcD1234", true),
+]
+
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            var accountController = new AccountController();
+
+            var actualResult = accountController.ValidatePassword(password);
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+
+
+        [
+        Test,
+        TestCase("irf@uni-corvinus.hu", "Abcd1234"),
+        TestCase("irf@uni-corvinus.hu", "Abcd1234567"),
+        ]
         public void TestRegisterHappyPath(string email, string password)
         {   
             var accountController = new AccountController();
@@ -45,6 +63,34 @@ namespace UnitTestExample.Test
             Assert.AreEqual(email, actualResult.Email);
             Assert.AreEqual(password, actualResult.Password);
             Assert.AreNotEqual(Guid.Empty, actualResult.ID);
+        }
+
+        [
+    Test,
+    TestCase("irf@uni-corvinus", "Abcd1234"),
+    TestCase("irf.uni-corvinus.hu", "Abcd1234"),
+    TestCase("irf@uni-corvinus.hu", "abcd1234"),
+    TestCase("irf@uni-corvinus.hu", "ABCD1234"),
+    TestCase("irf@uni-corvinus.hu", "abcdABCD"),
+    TestCase("irf@uni-corvinus.hu", "Ab1234"),
+]
+        public void TestRegisterValidateException(string email, string password)
+        {
+            
+            var accountController = new AccountController();
+
+            
+            try
+            {
+                var actualResult = accountController.Register(email, password);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOf<ValidationExpection>(ex);
+            }
+
+            // Assert
         }
 
 
